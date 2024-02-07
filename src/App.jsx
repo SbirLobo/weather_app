eed5a9e923aee0089b65567135ebd6119e6c2dd5
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2024-02-07 17:15:58 +0100

    ajout des fonctionnalités de weather API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import MyMapComponent from "./components/MyMapComponent";
+import WeatherInfos from "./components/WeatherInfos";
+import { useState } from "react";
 
 function App() {
+  const [lat, setLat] = useState(44.83741010727143);
+  const [lng, setLng] = useState(-0.5791063934285434);
+
   const VITE_GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
   const render = (status) => {
@@ -18,11 +23,17 @@ function App() {
 
   return (
     <>
-      <p className="text-3xl font-bold underline">Hello world!</p>
+      <main className="flex flex-col gap-8">
+        <p className="text-3xl font-bold underline">Weather app</p>
 
-      <Wrapper apiKey={VITE_GOOGLE_API_KEY} render={render}>
-        <MyMapComponent />
-      </Wrapper>
+        <Wrapper apiKey={VITE_GOOGLE_API_KEY} render={render}>
+          <MyMapComponent setLat={setLat} setLng={setLng}>
+            {/* <Marker position={latLng} /> */}
+          </MyMapComponent>
+        </Wrapper>
+
+        <WeatherInfos lat={lat} lng={lng} />
+      </main>
     </>
   );
 }
